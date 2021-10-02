d2e1f015ec9c60f760c2bc1cccdf13078c522535
Chloe <cguttweb@pm.me>
2021-10-02 17:48:01 +0100

    learning about code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js'
+    index: {
+      import: './src/index.js',
+      dependOn: 'shared'
+    },
+    print: {
+      import: './src/print.js',
+    },
+    new: {
+      import: './src/new-module.js',
+      dependOn: 'shared'
+    },
+    shared: 'lodash'
   },
   devtool: 'inline-source-map',
   devServer: {
@@ -13,8 +25,9 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development'
-    })
+      title: 'Code Splitting'
+    }),
+    new MiniCssExtractPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
@@ -22,11 +35,19 @@ module.exports = {
     clean: true,
     publicPath: '/',
   },
+  optimization: {
+    // This is needed if using multiple entry points on a single HTML page explanation here: https://bundlers.tooling.report/code-splitting/multi-entry/
+    // Although using multiple entry points per page is allowed in webpack, it should be avoided when possible in favor of an entry point with multiple imports: entry: { page: ['./analytics', './app'] }. This results in a better optimization and consistent execution order when using async script tag
+    // runtimeChunk: 'single'
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
   module: {
     rules: [
       {
         test: /\.(s|css)$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
