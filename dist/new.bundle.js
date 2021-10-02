3293cd2e201781fee2f0b92c88d877bb112264de
Chloe <cguttweb@pm.me>
2021-10-02 17:48:07 +0100

    learning about code splitting

---
+++ dist/new.bundle.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunkwebpack_demo"] = self["webpackChunkwebpack_demo"] || []).push([["new"],{
+
+/***/ "./src/new-module.js":
+/*!***************************!*\
+  !*** ./src/new-module.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);
+
+
+console.log(lodash__WEBPACK_IMPORTED_MODULE_0___default().join(['A new', 'module', 'loaded'], ' '))
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/new-module.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmV3LmJ1bmRsZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7QUFBc0I7O0FBRXRCLFlBQVksa0RBQU0iLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrLWRlbW8vLi9zcmMvbmV3LW1vZHVsZS5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgXyBmcm9tICdsb2Rhc2gnXG5cbmNvbnNvbGUubG9nKF8uam9pbihbJ0EgbmV3JywgJ21vZHVsZScsICdsb2FkZWQnXSwgJyAnKSkiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
